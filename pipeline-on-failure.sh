99b9091231e54c5dd71ee5ae71949744568d6c78
Igor Bronovskyi <admin@brun.if.ua>
2018-12-05 11:08:03 +0200

    Added functionality to run seeders

--- pipeline-on-failure.sh
+++ pipeline-on-failure.sh
@@ -13,6 +13,7 @@ minio-client config host add s3 http://127.0.0.1:9000 pipeline pipeline
 sleep 5 && minio-client mb s3/pipeline
 composer install --no-interaction --no-progress --prefer-dist
 php artisan migrate --force
+php artisan db:seed
 php artisan config:cache
 php artisan route:cache
 vendor/bin/phpunit -c phpunit.xml  --stop-on-failure tests/Backend
